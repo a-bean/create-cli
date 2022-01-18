b464d86a8ba9fb4491f89994c4bcdeace7cd967d
lizelong <lizelong@fuzhi.ai>
2022-01-18 10:50:30 +0800

    fix: prepare 添加文件权限设置

--- .husky/prepare.js
+++ .husky/prepare.js
@@ -3,4 +3,7 @@ if (!isCi) {
   /* eslint-disable @typescript-eslint/no-var-requires */
   // eslint-disable-next-line global-require
   require('husky').install();
+  const fs = require('fs');
+  const path = require('path');
+  fs.chmodSync(path.resolve(__dirname, './pre-commit'), '700');
 }
